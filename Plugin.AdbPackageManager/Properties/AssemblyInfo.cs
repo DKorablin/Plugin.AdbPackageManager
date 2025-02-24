a819fca6a0d3d50b8edf30dcf1784bb18e3dbac6
DKorablin2 <danila.korablin@unity3d.com>
2025-02-24 03:51:11 +0300

    Unify plugins deployment

--- Plugin.AdbPackageManager/Properties/AssemblyInfo.cs
+++ Plugin.AdbPackageManager/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("6d091f35-085e-4677-8e93-0d80a3733b95")]
 [assembly: System.CLSCompliant(true)]
 
@@ -9,14 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://github.com/DKorablin/Plugin.AdbPackageManager")]
 #else
 
-[assembly: AssemblyTitle("Plugin.AdbPackageManager")]
 [assembly: AssemblyDescription("UI for command tool adb.exe")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.AdbPackageManager")]
 [assembly: AssemblyCopyright("Copyright © Danila Korablin 2020-2024")]
 #endif
